4796303537d17ca5b55374ef0d2b16369235f78b
famanda <felicia.amanda@gmail.com>
2020-07-21 13:16:58 -0400

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        Project coded by {""}
-        <a href="https://www.linkedin.com/in/feliciaamanda">
+      <div className="container">
+        <h1>Weather App</h1>
+       <footer>
+          Project coded by {""}
+          <a href="https://www.linkedin.com/in/feliciaamanda">
           Felicia Amanda
-        </a>
-        {""} and is {""}
-      <a href="https://github.com/famanda/weather-react-app">
-        open-sourced on GitHub
-      </a>
-      </footer>
+          </a>
+          {""} and is {""}
+          <a href="https://github.com/famanda/weather-react-app">
+          open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
