1e0ae2c85e89961899156c7e06b4260fe5608447
famanda <felicia.amanda@gmail.com>
2020-07-21 11:34:50 -0400

    Added footer on App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>
+        Project coded by <a href="https://www.linkedin.com/in/feliciaamanda" target="_blank">Felicia Amanda</a>
+        {""} and is {""}
+      <a href="https://github.com/famanda/weather-react-app" target="_blank">open-sourced on GitHub</a>
+      </footer>
     </div>
   );
 }
-
-export default App;
